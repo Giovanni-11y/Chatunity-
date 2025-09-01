5c2fe353aad6c4f2ee2c9e6e351de8a155bc3552
darknet456 <albertozarro21@gmail.com>
2025-09-01 20:41:02 +0200

    Update tools-wiki.js

--- plugins/tools-wiki.js
+++ plugins/tools-wiki.js
@@ -1,35 +1,57 @@
 import axios from "axios"
 import fetch from "node-fetch"
 import cheerio from "cheerio"
-async function wikipedia(querry) {
-try {
-const link = await axios.get(`https://it.wikipedia.org/wiki/${querry}`)
-const $ = cheerio.load(link.data)
-let judul = $('#firstHeading').text().trim()
-let thumb = $('#mw-content-text').find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img').attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`
-let isi = []
-$('#mw-content-text > div.mw-parser-output').each(function (rayy, Ra) {
-let penjelasan = $(Ra).find('p').text().trim() 
-isi.push(penjelasan)})
-for (let i of isi) {
-const data = {
-status: link.status,
-result: {
-judul: judul,
-thumb: 'https:' + thumb,
-isi: i}}
-return data}
-} catch (err) {
-var notFond = {
-status: link.status,
-Pesan: eror}
-return notFond}}
+
+async function wikipedia(query) {
+  try {
+    const link = await axios.get(`https://it.wikipedia.org/wiki/${query}`)
+    const $ = cheerio.load(link.data)
+    
+    // Estrai il titolo della pagina Wikipedia
+    let title = $('#firstHeading').text().trim()
+    
+    // Estrai l'immagine di anteprima (o usa un'immagine di errore se non trovata)
+    let thumbnail = $('#mw-content-text')
+      .find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img')
+      .attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`
+    
+    // Estrai i paragrafi di testo dalla pagina
+    let content = []
+    $('#mw-content-text > div.mw-parser-output').each(function (index, element) {
+      let explanation = $(element).find('p').text().trim()
+      content.push(explanation)
+    })
+
+    // Restituisci il risultato come oggetto
+    for (let i of content) {
+      const data = {
+        status: link.status,
+        result: {
+          title: title,
+          thumbnail: 'https:' + thumbnail,
+          content: i
+        }
+      }
+      return data
+    }
+  } catch (err) {
+    var notFound = {
+      status: 404,
+      message: 'Error fetching Wikipedia data'
+    }
+    return notFound
+  }
+}
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return
-wikipedia(`${text}`).then(res => {
-m.reply(res.result.isi)
-}).catch(() => { return })}
-handler.help = ['wikipedia'].map(v => v + ' <apa>')
-handler.tags = [ 'internet']
+  if (!text) return
+  wikipedia(`${text}`).then(res => {
+    m.reply(res.result.content)
+  }).catch(() => { return })
+}
+
+handler.help = ['wikipedia'].map(v => v + ' <what>')
+handler.tags = ['internet']
 handler.command = /^(wiki|wikipedia)$/i
+
 export default handler
