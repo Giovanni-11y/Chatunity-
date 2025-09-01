e30c5d5ff42752839dd2d37138d55eef2e55f693
Giovanni-11y <giovanniferusso@gmail.com>
2025-09-01 14:10:51 +0200

    Update admin-deletesession.js

--- plugins/admin-deletesession.js
+++ plugins/admin-deletesession.js
@@ -3,8 +3,8 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(message.chat, {
-      text: "*🚨 𝐔𝐭𝐢𝐥𝐢𝐳𝐳𝐢 𝐪𝐮𝐞𝐬𝐭𝐨 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐝𝐢𝐫𝐞𝐭𝐭𝐚𝐦𝐞𝐧𝐭𝐞 𝐧𝐞𝐥 𝐧𝐮𝐦𝐞𝐫𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭.*"
+    return conn.sendMessage(m.chat, {
+      text: "*🚨 Use this command directly in the bot's number.*"
     }, { quoted: m });
   }
 
@@ -12,8 +12,8 @@ const handler = async (m, { conn, usedPrefix }) => {
     const sessionFolder = "./Sessioni/";
 
     if (!existsSync(sessionFolder)) {
-      return await conn.sendMessage(message.chat, {
-        text: "*❌ 𝐋𝐚 𝐜𝐚𝐫𝐭𝐞𝐥𝐥𝐚 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐞̀ 𝐯𝐮𝐨𝐭𝐚 o 𝐧𝐨𝐧 𝐞𝐬𝐢𝐬𝐭𝐞.*"
+      return await conn.sendMessage(m.chat, {
+        text: "*❌ The session folder is empty or does not exist.*"
       }, { quoted: m });
     }
 
@@ -27,16 +27,14 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
  
-    await conn.sendMessage(m.chat, { text: deletedCount === 0 ? '❗ 𝐋𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐬𝐨𝐧𝐨 𝐯𝐮𝐨𝐭𝐞, 𝐫𝐢𝐩𝐫𝐨𝐯𝐚 𝐭𝐫𝐚 𝐩𝐨𝐜𝐨 𝐧𝐞 𝐡𝐨 𝐛𝐢𝐬𝐨𝐠𝐧𝐨 ‼️' : '🔥 𝐒𝐨𝐧𝐨 𝐬𝐭𝐚𝐭𝐢 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐭𝐢 ' + deletedCount + ' 𝐚𝐫𝐜𝐡𝐢𝐯𝐢 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢! 𝐆𝐫𝐚𝐳𝐢𝐞 𝐩𝐞𝐫 𝐚𝐯𝐞𝐫𝐦𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐨😼'
-}, { quoted: m });
+    await conn.sendMessage(m.chat, { 
+      text: deletedCount === 0 
+        ? '❗ Sessions are empty, please try again later if you need them ‼️' 
+        : '🔥 ' + deletedCount + ' session files have been deleted! Thank you for clearing them out 😼'
+    }, { quoted: m });
   } catch (error) {
-    await conn.sendMessage(m.chat, { text: "❌ 𝐄𝐫𝐫𝐨𝐫𝐞 𝐝𝐢 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐳𝐢𝐨𝐧𝐞!" }, { quoted: m });
+    await conn.sendMessage(m.chat, { text: "❌ Error during deletion!" }, { quoted: m });
   }
 };
-
-handler.help = ['del_reg_in_session_owner'];
-handler.tags = ["owner"];
-handler.command = /^(deletession|ds)$/i;
-handler.admin = true;
 
 export default handler;
