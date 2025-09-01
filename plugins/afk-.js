a9421d585f171f705c4ef08e3fb0f3602d8c4659
Giovanni-11y <giovanniferusso@gmail.com>
2025-09-01 14:14:10 +0200

    Update afk-.js

--- plugins/afk-.js
+++ plugins/afk-.js
@@ -1,20 +1,21 @@
-let handler = async (m, { text, args, usedPrefix, command, conn}) => { 
-    let user = global.db.data.users[m.sender]
+let handler = async (m, { text, args, usedPrefix, command, conn }) => { 
+    let user = global.db.data.users[m.sender];
       
     if (args.length >= 1) {
-    text = args.slice(0).join(" ")
+        text = args.slice(0).join(" ");
     } else if (m.quoted && m.quoted.text) {
-    text = m.quoted.text
-    } else return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
-      
-    if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
-    user.afk = + new Date
-    user.afkReason = text
-    await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
-    *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-    ${lenguajeGB['smsAfkM1A']()} *@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1B']()}${text ? '\n👉 ' + text : ''}`, m, { mentions: [m.sender] })
-    }
+        text = m.quoted.text;
+    } else return m.reply(`Use: ${usedPrefix + command} <reason>`); // smsAfkQ1
     
-    handler.command = /^afk$/i
-    handler.register = true
-    export default handler
+    if (text.length < 10) return m.reply(`Please provide a longer reason for being AFK.`); // smsAfkQ2
+    
+    user.afk = + new Date;
+    user.afkReason = text;
+    await conn.reply(m.chat, `🔔 *AFK Activated* 🔔
+*------------------------------*
+Hey! @${m.sender.split("@")[0]} is now AFK.${text ? '\n👉 ' + text : ''}`, m, { mentions: [m.sender] }); // smsAvisoAG, smsAfkM1A, smsAfkM1B
+}
+    
+handler.command = /^afk$/i;
+handler.register = true;
+export default handler;
