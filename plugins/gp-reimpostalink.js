811d23a05f5293051883b4330abc4be706fcfff4
darknet456 <albertozarro21@gmail.com>
2025-09-02 22:51:26 +0200

    Update gp-reimpostalink.js

--- plugins/gp-reimpostalink.js
+++ plugins/gp-reimpostalink.js
@@ -1,25 +1,24 @@
-
-let handler = async(m, { conn }) => {
-    let nomeDelBot = global.db.data.nomedelbot || `𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲`
-    let revoke = await conn.groupRevokeInvite(m.chat)
+let handler = async (m, { conn }) => {
+    let botName = global.db.data.botName || `𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲`;
+    let revoke = await conn.groupRevokeInvite(m.chat);
     
     await conn.sendMessage(m.chat, {
-      text: `🔹️ *link reimpostato*\n♾ • ID: ${'https://chat.whatsapp.com/' + revoke}`,
+      text: `🔹️ *link reset*\n♾ • ID: ${'https://chat.whatsapp.com/' + revoke}`,
       contextInfo: {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363259442839354@newsletter',
           serverMessageId: '',
-          newsletterName: `${nomeDelBot}`
+          newsletterName: `${botName}`
         }
       }
-    })
-  }
+    });
+}
   
-  handler.command = ['reimposta', 'revoke']
-  handler.botAdmin = true
-  handler.admin = true
-  handler.group = true
+handler.command = ['reset', 'revoke'];
+handler.botAdmin = true;
+handler.admin = true;
+handler.group = true;
   
-  export default handler
+export default handler;
