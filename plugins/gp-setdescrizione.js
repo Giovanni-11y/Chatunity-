b062149d49cfa71bcf598aa15c1c31ddaa7f51e9
GabWT333 <gab333github@gmail.com>
2025-09-02 09:19:20 +0200

    Update gp-setdescrizione.js

--- plugins/gp-setdescrizione.js
+++ plugins/gp-setdescrizione.js
@@ -1,32 +1,29 @@
 const handler = async (m, { conn, args }) => {
     try {
-      
-      const pp = await conn.profilePictureUrl(m.chat, 'image').catch(() => './menu/chatunitybot.mp4');
-      
-      // Unisci gli argomenti per creare la descrizione
-      const description = args.join(' ').trim();
-      
-      if (!description) {
-        return conn.reply(m.chat, 'Per favore, inserisci una descrizione valida.', m);
-      }
-  
-      // Aggiorna la descrizione del gruppo
-      await conn.groupUpdateDescription(m.chat, description);
-      
-      // Invia conferma
-      await conn.reply(m.chat, '✅ *Descrizione del gruppo aggiornata con successo!*', m);
-      
+
+        const pp = await conn.profilePictureUrl(m.chat, 'image').catch(() => './menu/chatunitybot.mp4');
+
+        const description = args.join(' ').trim();
+
+        if (!description) {
+            return conn.reply(m.chat, 'Please enter a valid group description.', m);
+        }
+
+        await conn.groupUpdateDescription(m.chat, description);
+
+        await conn.reply(m.chat, '✅ *Group description updated successfully!*', m);
+
     } catch (error) {
-      console.error('Errore nel cambiare la descrizione:', error);
-      conn.reply(m.chat, '❌ *Si è verificato un errore durante l\'aggiornamento della descrizione.*', m);
+        console.error('Error updating group description:', error);
+        conn.reply(m.chat, '❌ *An error occurred while updating the description.*', m);
     }
-  }
-  
-  handler.help = ['setdesc <testo>'];
-  handler.tags = ['group'];
-  handler.command = /^setdesk|setdesc(rizione)?|descrip(ción|cion)$/i;
-  handler.group = true;
-  handler.admin = true;
-  handler.botAdmin = true;
-  
-  export default handler;
+}
+
+handler.help = ['setdesc <text>'];
+handler.tags = ['group'];
+handler.command = /^setdesk|setdesc(rizione)?|descrip(ción|cion)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
