968cfe54b72e0e9c11622a301d414718cefc9f52
Giovanni-11y <giovanniferusso@gmail.com>
2026-04-02 11:31:20 +0200

    Delete plugins directory

--- plugins/info-funzionitotali.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-    let totalFunctions = Object.values(global.plugins).filter(v => v.command).length;
-    let num1 = ['𝟏', '𝟐', '𝟑', '𝟒', '𝟓', '𝟔', '𝟕', '𝟖', '𝟗'];
-    let num2 = totalFunctions.toString().split('').map(digit => num1[digit]).join('');
-    m.reply(`ⓘ The bot has ${num2} functions.`);
-}
-
-handler.command = ['totalfunctions', 'functions'];
-export default handler;
